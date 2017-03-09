728952cf5330293c61be4bb7acc7b51c86e951d3
Vincent Euloge <vincent.euloge@gmail.com>
2017-03-09 22:08:45 +0100

    const

---
+++ const.js
@@ -0,0 +1,8 @@
+// un let dont on ne peut modifier la référence
+let a = 10;
+a = 11; // pas de soucis
+
+const b = 10;
+b = 10; // erreur
+
+// je parle bien de reference car si on stock un objet dans notre const on pourra changer le contenu de l'objet
